b4cffd01fc494b7633f6fc06adc403c9b1f30feb
KennethEladistu <kenneth.eladistu@planate.com>
2026-05-12 10:20:45 +0800

    configured moveit for humble

--- trossen_arm_controllers/src/gravity_compensation.cpp
+++ trossen_arm_controllers/src/gravity_compensation.cpp
@@ -89,11 +89,15 @@ GravityCompensationController::update(
   const rclcpp::Duration & /*period*/)
 {
   // Set all command interfaces to zero external effort
+  // for (auto & command_interface : command_interfaces_) {
+  //   if (!command_interface.set_value(0.0)) {
+  //     RCLCPP_ERROR(get_node()->get_logger(), "Failed to set command interface value");
+  //     return return_type::ERROR;
+  //   }
+  // }
+  // return return_type::OK;
   for (auto & command_interface : command_interfaces_) {
-    if (!command_interface.set_value(0.0)) {
-      RCLCPP_ERROR(get_node()->get_logger(), "Failed to set command interface value");
-      return return_type::ERROR;
-    }
+    command_interface.set_value(0.0);
   }
   return return_type::OK;
 }
--- trossen_arm_hardware/include/trossen_arm_hardware/interface.hpp
+++ trossen_arm_hardware/include/trossen_arm_hardware/interface.hpp
@@ -155,10 +155,10 @@ protected:
   bool gripper_effort_mode_running_{false};
 
   // Logger
-  // static rclcpp::Logger get_logger()
-  // {
-  //   return rclcpp::get_logger("trossen_arm_hardware");
-  // }
+  static rclcpp::Logger get_logger()
+  {
+    return rclcpp::get_logger("trossen_arm_hardware");
+  }
 
   /**
    * @brief Check if the interface type is in the stop interfaces
